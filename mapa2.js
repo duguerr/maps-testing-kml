f1fb833e8dbbdc39d07fe50cfbe0e4331e43e173
ericmtzmtz <52549279+ericmtzmtz@users.noreply.github.com>
2021-07-27 20:59:35 -0500

    Working

--- mapa2.js
+++ mapa2.js
@@ -1,78 +1,74 @@
+var map;
+var src = 'http://lorimapstestinapp.atwebpages.com/tres.kmz';
+
+styles = [
+  {
+    "featureType": "administrative",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "transit",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  }
+];
+
 function initMap() {
-    
-    var MapOptions = {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: new google.maps.LatLng(6.243007792147181, -75.58035376905785),
     zoom: 12,
-    scaleControl: true,
-    
-    center: {
-        
-    
-        lat: 6.243007792147181,
-        lng: -75.58035376905785
-    },
-    
-    
-    styles: [
-        {
-            "featureType": "administrative",
-            "elementType": "geometry",
-            "stylers": [
-                {
-                    "visibility": "off"
-                }
-            ]
-        },
-        {
-            "featureType": "poi",
-            "stylers": [
-                {
-                    "visibility": "off"
-                }
-            ]
-        },
-        {
-            "featureType": "road",
-            "elementType": "labels.icon",
-            "stylers": [
-                {
-                    "visibility": "off"
-                }
-            ]
-        },
-        {
-            "featureType": "transit",
-            "stylers": [
-                {
-                    "visibility": "off"
-                }
-            ]
-        }
-    ]   
-};
-    
-    
-    var map = new google.maps.Map(document.getElementById("map"), MapOptions);
-    
-    
-    
-    /*
-    //cuestion los archivos se llaman uno.kmz   dos.kmz     tres.kmz
-     var ctaLayer = new google.maps.KmlLayer({
-            url: "http://lorimapstestinapp.atwebpages.com/tres.kmz",
-            map: map
-        });//fin de la variable ctalayer  
-   
-    */
-    
-}//fin del la funcion initMap
+    mapTypeId: 'terrain',
+    styles: styles
+  });
+
+  const kmlLayer = (src2) => new google.maps.KmlLayer(src2, {
+    suppressInfoWindows: true,
+    preserveViewport: false,
+    map: map
+  });
 
 
+  document.getElementById('boton1').addEventListener('click', (e) => {
+    src = 'http://lorimapstestinapp.atwebpages.com/uno.kmz';
+    kmlLayer(src)
+  })
 
+  document.getElementById('boton2').addEventListener('click', (e) => {
+    src = 'http://lorimapstestinapp.atwebpages.com/dos.kmz';
+    kmlLayer(src)
+  });
 
-   
+  document.getElementById('boton3').addEventListener('click', (e) => {
+    src = 'http://lorimapstestinapp.atwebpages.com/tres.kmz';
+    kmlLayer(src)
+  });
 
-
-
-
-
-
+  window.onload = function() {
+    // kmlLayer(src)
+  };
+}
